ffcb44da1277919a2c9aa5890b394b8209731ec0
gelerli <82060355+gelerli@users.noreply.github.com>
2026-08-13 02:07:14 +0300

    fix footer rollback

--- ui/footer.py
+++ ui/footer.py
@@ -63,7 +63,7 @@ class Footer:
         )
 
         font = get_font_small()
-        padding = 40
+        padding = 10
         cy = self.rect.centery
 
         # 1. Version String (Left): "DIRT-TOUCH" in orange, "-v0.3" in muted brown
