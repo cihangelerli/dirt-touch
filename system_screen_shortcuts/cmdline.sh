1ad1f621a541a3ca32068103d323c0a17fb68611
Cihan Gelerli <cihan2gelerli@gmail.com>
2026-08-12 22:57:20 +0300

    add system screen shortcuts

---
+++ system_screen_shortcuts/cmdline.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export SDL_VIDEODRIVER="${SDL_VIDEODRIVER:-kmsdrm}"
+export SDL_MOUSEDRV="${SDL_MOUSEDRV:-evdev}"
+
+STTY_BAK=$(stty -g 2>/dev/null)
+
+cleanup() {
+    if [ -n "$STTY_BAK" ]; then
+        stty "$STTY_BAK" 2>/dev/null
+    fi
+    stty echo sane 2>/dev/null
+    setterm -cursor on 2>/dev/null
+    tput cnorm 2>/dev/null
+}
+
+trap cleanup EXIT INT TERM
+
+# Reset console state and show cursor
+stty sane 2>/dev/null
+setterm -cursor on 2>/dev/null
+tput cnorm 2>/dev/null
+
+# Allocate PTY for nano cmdline.txt
+python3 -c "import pty; pty.spawn(['sudo', 'nano', '/boot/firmware/cmdline.txt'])"
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -eq 0 ] || [ $EXIT_CODE -eq 130 ] || [ $EXIT_CODE -eq 143 ]; then
+    exit 0
+fi
+
+exit $EXIT_CODE
+
